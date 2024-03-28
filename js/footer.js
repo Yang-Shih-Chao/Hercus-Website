696d82197ca39977e050aa7ddc884ab7f8de52e4
咘筱芯 <yang.shih.chao@gmail.com>
2024-03-28 17:20:38 +0800

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,10 @@
+footer = 
+'<footer id="footer">\
+        <div>\
+            <p>&copy; Designed & Copyright by Hercus Yang 2023</p>\
+        </div>\
+        <div style="text-align: right;">\
+            <p>歡迎合作邀約</p>\
+        </div>\
+</footer>';
+document.write(footer);
---
+++ js/header.js
@@ -0,0 +1,43 @@
+header=
+'<header>\
+<div id="web-logo">\
+    <a href="./">\
+        <img src="./images/header-logo.png">\
+    </a>\
+</div>\
+</header>\
+<nav>\
+<div>\
+    <a href="./index.html">\
+        <h5>About me</h5>\
+    </a>\
+</div>\
+<div>\
+    <a href="./projects.html">\
+        <h5>Projects</h5>\
+    </a>\
+</div>\
+<div>\
+    <a href="./articles.html">\
+        <h5>Articles</h5>\
+    </a>\
+</div>\
+<div>\
+    <a href="./contact.html">\
+        <h5>Contact</h5>\
+    </a>\
+</div>\
+<div class="sociolMedia">\
+    <span class="facebook sociolMedia-logo">\
+        <a href="https://www.facebook.com"><img src="./images/facebook-logo.png" alt="facebook-Logo"></a>\
+    </span>\
+    <span class="instagram sociolMedia-logo">\
+        <a href="https://www.instagram.com/"><img src="./images/instagram-logo.png" alt="instagram-Logo"></a>\
+    </span>\
+    <span class="github sociolMedia-logo">\
+        <a href="https://github.com/Yang-Shih-Chao"><img src="./images/github-logo.png" alt="github-Logo"></a>\
+    </span>\
+</div>\
+</nav>\
+';
+document.write(header);
